4249dded9c98aa8801a39344b0beb8f0b399d1e1
Joe Romeo <45112027+JoeFromDenver@users.noreply.github.com>
2020-08-31 19:16:45 -0600

    Export from codepen.io
    Been running my resume on a live site there for ages. Time to bring it to GitHub.

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Resume
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/JoeRomeo/pen/pqyjRR](https://codepen.io/JoeRomeo/pen/pqyjRR).
+
+Online resume for Joe Romeo
---
+++ script.js
@@ -0,0 +1,43 @@
+// Some code thanks to @chrisgannon
+
+var select = function(s) {
+  return document.querySelector(s);
+}
+
+function randomBetween(min,max)
+{
+    var number = Math.floor(Math.random()*(max-min+1)+min);
+  
+    if ( number !== 0 ){
+      return number;
+    }else {
+      return 0.5;
+    }
+}
+
+var tl = new TimelineMax();
+
+for(var i = 0; i < 20; i++){
+
+  var t = TweenMax.to(select('.bubble' + i), randomBetween(1, 1.5), {
+    x: randomBetween(12, 15) * (randomBetween(-1, 1)),
+    y: randomBetween(12, 15) * (randomBetween(-1, 1)), 
+    repeat:-1,
+    repeatDelay:randomBetween(0.2, 0.5),
+    yoyo:true,
+    ease:Elastic.easeOut.config(1, 0.5)
+  })
+
+  tl.add(t, (i+1)/0.6)
+}
+
+tl.seek(50);
+
+$('a')
+		.filter('[href^="http"], [href^="//"]')
+		.not('[href*="' + window.location.host + '"]')
+		.attr('rel', 'noopener noreferrer')
+		.attr('target', '_blank')
+    .css('color', 'red');
+
+console.log(window.location.host)
